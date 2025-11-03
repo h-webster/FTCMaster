88d21b81156ace420ad44da12e2a1ddcef9cb5f4
harrisonwebster <119012781+h-webster@users.noreply.github.com>
2025-11-03 10:53:50 -0700

    fix

--- backend/api/server.js
+++ backend/api/server.js
@@ -21,7 +21,7 @@ const app = express();
 // Middleware
 app.use(cors({
     origin: process.env.NODE_ENV === 'production'
-    ? ['https://ftc-master-b59g.vercel.app/', '']
+    ? ['https://ftc-master-b59g.vercel.app']
     : ['http://localhost:3000', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
