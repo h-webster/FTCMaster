71719e0ac0cabaaff4035eb082a66b023a552c0a
harrisonwebster <119012781+h-webster@users.noreply.github.com>
2025-11-23 22:22:53 -0700

    need to use home pc to setup open api

--- src/api/pulling/ExtraGetting.jsx
+++ src/api/pulling/ExtraGetting.jsx
@@ -4,16 +4,34 @@ export const useExtraGetting = () => {
     const { teamData } = useData();
     const extraDataExtraction = async (teamNum) => {
         console.log(`Getting extra data for team ${teamNum}...`);
+        formatForAI();
+    }
+
+    const removeFieldsJSON = (obj, fieldName) => {
+        const jsonString = JSON.stringify(obj, (key, value) => {
+            if (key === fieldName) return undefined;
+            return value;
+        });
+        return JSON.parse(jsonString);
     }
 
     const formatForAI = () => {
         let formatted = {
-            teamInfo: {
-                name: teamData.name,
-            },
-
-            name: teamData.name
+            info: teamData.info,
+            performance: teamData.performance,
+            opr: teamData.opr,
+            pointAverage: teamData.pointAverage,
         };
+        let events = [];
+        for (let event of teamData.events) {
+            if (event.matches.length > 0) {
+                events.push(...event.matches);
+            }
+        }
+        formatted.events = events;
+        let removedFormatted = removeFieldsJSON(formatted, "_id");
+        removedFormatted = removeFieldsJSON(removedFormatted, "__v");
+        console.log(removedFormatted);
     }
     return { extraDataExtraction };
 };
