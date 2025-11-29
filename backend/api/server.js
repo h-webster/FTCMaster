cfe2c7145d7d631023c72414025f537210eaf214
Wonkanese <northkoreanwillywonka@gmail.com>
2025-11-28 21:55:25 -0700

    hosting fix

--- backend/api/server.js
+++ backend/api/server.js
@@ -24,7 +24,7 @@ const app = express();
 // Middleware
 app.use(cors({
     origin: process.env.NODE_ENV === 'production'
-    ? ['https://ftc-master-b59g.vercel.app']
+    ? ['https://ftc-master-b59g.vercel.app', 'https://www.ftcmaster.org']
     : ['http://localhost:3000', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
