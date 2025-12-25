c67a2b105671473c4f084a00c146e812f1fbea44
harrisonwebster <119012781+h-webster@users.noreply.github.com>
2025-12-25 15:55:18 -0700

    cleaner look

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Header.css';
 import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGears} from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
   const navigate = useNavigate();
@@ -14,7 +16,8 @@ export default function Header() {
 
   return (
     <div className='header'>
-      <img className='big-img' src="/ftcmaster.png"></img>
+      <img className="big-img" src="bot.png"/>
+      <h1 className='big-text'>FTCMaster</h1>
       <button className='home-btn' onClick={goHome}>Home</button>
       <button className='home-btn' onClick={goAbout}>About</button>
     </div>
--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,13 +1,24 @@
 import './LoadingScreen.css';
 import React from 'react';
 import { useData } from '../contexts/DataContext';
+import Header from './Header';
+import { ClipLoader } from "react-spinners";
 
 export default function LoadingScreen() {
     const { loadingStatus } = useData();
     return (
-        <div className="loading-screen">
-            <h2>Loading...</h2>
-            <p>{loadingStatus}</p>
-        </div>
+        <>
+            <Header />
+            <div className="loading-screen">
+                <ClipLoader
+                    color={"#2fe06aff"}
+                    loading={true}
+                    size={150}
+                    aria-label="Loading Spinner"
+                    data-testid="loader"
+                />
+                <p>{loadingStatus}</p>
+            </div>
+        </>
     );
 }
